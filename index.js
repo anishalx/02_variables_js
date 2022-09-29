ba40db81a1a6fa2bd0af26d1e7ec8f060b4b5896
Anish <113669810+radlysamar@users.noreply.github.com>
2022-09-29 17:27:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("hello this is a replit which i never used before")
+let a = 67 //a contains 67
+console.log(a)
+a = "anish"
+console.log(a)
+let $anish = 10 //start with no is not allowed it returns an error
+console.log($anish)
+//let var=2//
+//console.log(var)//
